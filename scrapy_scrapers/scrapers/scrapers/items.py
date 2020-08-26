03cd2f7baebbfe445920dcc12c9503af3110790e
Steffi Choi <steffic.06@gmail.com>
2020-08-26 16:14:08 -0400

    amazon scraper with generated csv file

---
+++ scrapy_scrapers/scrapers/scrapers/items.py
@@ -0,0 +1,20 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class AmazonMainPageItem(scrapy.Item):
+    item_name = scrapy.Field()
+    item_link = scrapy.Field()
+    item_price = scrapy.Field()
+
+class AmazonItem(scrapy.Item):
+    product_name = scrapy.Field()
+    product_sale_price = scrapy.Field()
+    product_category = scrapy.Field()
+    product_original_price = scrapy.Field()
+    product_availability = scrapy.Field()
+
+
---
+++ scrapy_scrapers/scrapers/scrapers/spiders/AmazonItemsSpider.py
@@ -0,0 +1,27 @@
+import scrapy
+from ..items import AmazonMainPageItem
+
+class AmazonItemsSpider(scrapy.Spider):
+    name = 'AmazonItemsSpider'
+    allowed_domains = ['amazon.ca']
+    start_urls = [
+        "https://www.amazon.ca/s?k=weights&ref=nb_sb_noss_2"
+        ]
+
+    def parse(self, response):
+
+        mainpage = response.xpath("//*[@class='s-expand-height s-include-content-margin s-border-bottom s-latency-cf-section']")
+
+        for item in mainpage:
+            itm = AmazonMainPageItem()
+
+            itm_title = item.xpath('.//h2[@class="a-size-mini a-spacing-none a-color-base s-line-clamp-4"]/a/span/text()').extract_first()
+            itm_link = item.xpath('.//h2[@class="a-size-mini a-spacing-none a-color-base s-line-clamp-4"]/a/@href').extract_first()
+            itm_wholeprice = item.xpath('.//span[@class="a-price"]/span/text()').extract_first()
+
+            if itm_title and itm_link:
+                itm['item_name'] = ''.join(itm_title).strip()
+                itm['item_link'] = ''.join(itm_link).strip()
+                if itm_wholeprice:
+                    itm['item_price'] = ''.join(itm_wholeprice).strip()
+                yield itm
